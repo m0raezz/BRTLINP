08789bae771bf908d5cea4ee7e7a4adcf6b033e2
m0raezz <71576605+m0raezz@users.noreply.github.com>
2025-03-18 21:18:02 -0300

    Add files via upload

---
+++ ATV_6_BRTLIPN/ATV3.c
@@ -0,0 +1,36 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+        
+        float lado1, lado2, lado3;
+        char * = "Seu triangulo retangulo e possivel";
+        
+        while(numero != -1) {
+            printf("Digite um lado (-1 para sair): ");
+            scanf("%f \n", &lado1);
+            if(lado1 == -1){ numero = -1};
+            
+            printf("Digite outro lado: ");
+            scanf("%f \n", &lado2);
+            
+            printf("Digite outro lado : ");
+            scanf("%f", &lado3);
+            
+            
+            if(lado1 > lado2){
+                if(lado2 > lado3){
+                    float hipo = lado1
+                    if((lado2 * lado2) + (lado3 * lado3) == (lado1 * lado1)){
+                        
+                    }
+                    
+                }
+            }
+            
+            
+
+          }
+        
+
+    return 0;

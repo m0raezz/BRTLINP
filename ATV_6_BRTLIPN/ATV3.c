98550bc3d2fe2c4841486ecfcd71a43afb1f7388
m0raezz <71576605+m0raezz@users.noreply.github.com>
2025-03-18 21:20:35 -0300

    Update ATV3.c

--- ATV_6_BRTLIPN/ATV3.c
+++ ATV_6_BRTLIPN/ATV3.c
@@ -2,35 +2,68 @@
 #include <stdio.h>
 
 int main() {
+    float lado1, lado2, lado3;
+    char *frase = "Seu triangulo retangulo e possivel";
+    int contador = 1;
+    
+    while (contador != -1) {
+        puts("Digite um lado (-1 para sair): ");
+        scanf("%f", &lado1);  
         
-        float lado1, lado2, lado3;
-        char * = "Seu triangulo retangulo e possivel";
-        
-        while(numero != -1) {
-            printf("Digite um lado (-1 para sair): ");
-            scanf("%f \n", &lado1);
-            if(lado1 == -1){ numero = -1};
+        if (lado1 == -1) { 
+            contador = -1;
+        }else{
+
+        if (lado1 != -1) {  
+            printf("Digite outro lado: ");
+            scanf("%f", &lado2);  
             
             printf("Digite outro lado: ");
-            scanf("%f \n", &lado2);
-            
-            printf("Digite outro lado : ");
-            scanf("%f", &lado3);
-            
-            
-            if(lado1 > lado2){
-                if(lado2 > lado3){
-                    float hipo = lado1
-                    if((lado2 * lado2) + (lado3 * lado3) == (lado1 * lado1)){
-                        
+            scanf("%f", &lado3);  
+
+
+            if (lado1 > lado2) {
+                if (lado1 > lado3) {
+
+                    if ((lado2 * lado2) + (lado3 * lado3) == (lado1 * lado1)) {
+                        printf("%s\n", frase);  
+                        contador = -1;
+                    }else{
+                        puts("Triangulo invalido");
                     }
-                    
                 }
             }
-            
-            
 
-          }
+
+            if (lado2 > lado1) {
+                if (lado2 > lado3) {
+
+                    if ((lado1 * lado1) + (lado3 * lado3) == (lado2 * lado2)) {
+                        printf("%s\n", frase);  
+                        contador = -1;
+                    }else{
+                        puts("Triangulo invalido");
+                    }
+                }
+            }
+
+            
+            if (lado3 > lado1) {
+                if (lado3 > lado2) {
+                    
+                    if ((lado1 * lado1) + (lado2 * lado2) == (lado3 * lado3)) {
+                        printf("%s\n", frase);  // Adiciona uma nova linha após a frase
+                        contador = -1;
+                    }else{
+                        puts("Triangulo invalido");
+                    }
+                }
+            }
         
+        }
+    
+    }            
+}
 
     return 0;
+}

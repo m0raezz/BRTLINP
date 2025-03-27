b9751926f06c86c4e163afc269af63e1a342d814
m0raezz <71576605+m0raezz@users.noreply.github.com>
2025-03-26 21:42:58 -0300

    Update ATV3.c

--- ATV_7_BRTLINP/ATV3.c
+++ ATV_7_BRTLINP/ATV3.c
@@ -2,31 +2,35 @@
 
 int main(void) {
     char tamanho;
-    int estado = 0;
 
   printf("Digite a operação desejada (S |SECAR  E| ENXUGAR  L| LAVAR || F para encerrar):");
   scanf("%c", &tamanho);
+  getchar();
 
-
+while( tamanho != 'F'){
   
     switch (tamanho) {
       case 'L':
-    	estado++;
+    	printf("\nSua roupa foi LAVADA, ENXAGUADA e SECADA\n");
+    	break;
       case 'E': 
-    	estado++;
+    	printf("\nSua roupa foi ENXAGUADA e SECADA\n");
+    	break;
       case 'S':
-    	estado++;
+    	printf("\nSua roupa foi apenas SECADA\n");
+	    break;
+	  default:
+	    printf("Opcao nao encontrada!");
 	    break;
     }
-
-  if(estado == 1){
-      printf("Sua roupa foi apenas SECADA");
-  }
-  if(estado == 2){
-      printf("Sua roupa foi ENXAGUADA e SECADA");
-  }
-  if(estado == 3){
-      printf("Sua roupa foi LAVADA, ENXAGUADA e SECADA");
-  }
+    
+     
+      
+  printf("Digite a operação desejada (S |SECAR  E| ENXUGAR  L| LAVAR || F para encerrar):");
+  scanf("%c", &tamanho);
+  getchar();
+    
+}
+printf("\nProcesso encerrado!");
   return 0;
 }

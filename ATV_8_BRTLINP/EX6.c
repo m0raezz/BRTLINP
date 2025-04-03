cf936dee909840360235d592c6e32276db5579c7
m0raezz <71576605+m0raezz@users.noreply.github.com>
2025-04-02 21:13:51 -0300

    Create EX6.c

---
+++ ATV_8_BRTLINP/EX6.c
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+        for(int a = 0; a < 2; a++){
+                printf("A:%d! = %d\n", a, !a);
+        
+        }
+        
+        
+        
+
+    return 0;
+}

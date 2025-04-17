99dca4b3192fa5a7700f5e606efb1c2e0c640733
m0raezz <71576605+m0raezz@users.noreply.github.com>
2025-04-16 21:46:09 -0300

    Update scan.h

--- ATV_A10_BRTLINP/ATV5/scan.h
+++ ATV_A10_BRTLINP/ATV5/scan.h
@@ -1,24 +1,11 @@
 #include <stdio.h>
 
-void scan(int *x, int *y, int *z){
-    printf("Digite o primeiro numero: ");
+void scan(int *x){
+    printf("Digite o numero: ");
     scanf("%d", &*x);
-    
-    printf("Digite o primeiro numero: ");
-    scanf("%d", &*y);
-
-    printf("Digite o primeiro numero: ");
-    scanf("%d", &*z);
 
     *x = *x;
-    *y = *y;
-    *z = *z;
 
-
-    printf("Primeiro numero: %d\n", *x);
-    printf("Segundo numero: %d\n", *y);
-    printf("Terceiro numero: %d\n", *z);
-
-
+    printf("Numero digitado: %d\n\n", *x);
 
 }

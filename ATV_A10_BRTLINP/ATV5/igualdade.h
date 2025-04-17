56a1201a62b3921f6757302596fea099ea19fafb
m0raezz <71576605+m0raezz@users.noreply.github.com>
2025-04-16 21:32:50 -0300

    Update igualdade.h

--- ATV_A10_BRTLINP/ATV5/igualdade.h
+++ ATV_A10_BRTLINP/ATV5/igualdade.h
@@ -29,14 +29,3 @@ int menor(int x, int y){
 }
 
 
-int maior2(int x, int y, int z){
-
-    if (x > y || x > z) {
-        return x;
-    }else if (y > x || y > z) {
-        return y;
-    }else if (z > x || z > y) {
-        return z;
-    }
-
-}

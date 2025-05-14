cebe81313a9c4c1958fbd99c090db1a388f9139f
m0raezz <71576605+m0raezz@users.noreply.github.com>
2025-05-14 20:02:11 -0300

    Add files via upload

---
+++ ATV_A13_BRTLINP/EX4/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    
+    int altura;
+    printf("Digite a altura desejada: ");
+    scanf("%d", &altura);
+    for (int i = 1; i <= altura; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf(" ⭐ ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
---
+++ ATV_A13_BRTLINP/EX5/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    int altura;
+    char rsp;
+
+    do{
+        printf("Digite a altura desejada: "); 
+        scanf(" %d", &altura);
+
+        if(altura < 1||rsp=='F') break;
+
+        printf("Desenhar à esquerda(A) ou direita(D)? (F para sair): ");
+        scanf(" %c", &rsp);
+
+        if(rsp == 'F') break;
+
+        if (rsp == 'D') {
+            for (int i = 1; i <= altura; i++) {
+                for (int j = 1; j <= i; j++) {
+                    printf("⭐");
+                }
+                printf("\n");
+            }
+        } else if (rsp == 'A') {
+            for (int i = 1; i <= altura; i++) {
+                for (int j = 1; j <= altura - i; j++) {
+                    printf(" ");
+                }
+                for (int j = 1; j <= i; j++) {
+                    printf("⭐");
+                }
+                printf("\n");
+            }
+        } else {
+            printf("Opção inválida. Escolha A ou D.\n");
+        }
+    }while(rsp != 'F');
+
+    return 0;
+}

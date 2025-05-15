41223ae2fe1470bfcb426596f1b6befa075795a7
m0raezz <71576605+m0raezz@users.noreply.github.com>
2025-05-14 21:34:15 -0300

    Update main.c

--- ATV_A13_BRTLINP/EX4/main.c
+++ ATV_A13_BRTLINP/EX4/main.c
@@ -7,7 +7,7 @@ int main(){
     scanf("%d", &altura);
     for (int i = 1; i <= altura; i++) {
         for (int j = 1; j <= i; j++) {
-            printf(" ⭐ ");
+            printf("*");
         }
         printf("\n");
     }
